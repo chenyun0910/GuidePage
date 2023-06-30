58123d9a8ff1e4b0aeed13f9774c2462c6135240
陈允 <cheny@ijia120.com>
2023-06-30 15:35:29 +0800

    初始化 添加引导页

--- GuidePage/ViewController.swift
+++ GuidePage/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+       let pageIndex =  round(scrollView.contentOffset.x / view.bounds.width)
+       pageControl.currentPage = Int(pageIndex)
+        if scrollView.contentOffset.x > (view.bounds.width * 2 + 50) {
+            var sb = storyboard!.instantiateInitialViewController()!
+            sb.modalPresentationStyle = .fullScreen
+            present(sb, animated: true)
+        }
+    }
 }
 
